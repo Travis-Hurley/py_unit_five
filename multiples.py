fcdf1aa892dac26f1c6786ab19dbe3ffeaecee9b
Travis-Hurley <hurlean@ssfs.org>
2022-11-17 12:02:28 -0500

    Update multiples.py

--- multiples.py
+++ multiples.py
@@ -1,10 +1,8 @@
 def get_sum(number):
-    """
-    Will return the sum of all the multiples of 3 or 5 of the given number.
-    Ex. get_sum(10) returns 23
-    :param number: The value up to which we want to find the sum. Not included in the calculation
-    :return: The sum of all the multiples of 3 or 5 up to the number.
-    """
-    pass # make sure to delete this line when writing your function
-
+    sum=0
+    for x in range(number):
+        if x % 3 == 0 or x % 5 == 0:
+            sum += (x)
+    print(sum)
+get_sum(10)
 
